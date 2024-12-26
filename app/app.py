bd87ea1b7217bb8a6830d050bd17b008c2f4a675
# PWPrivaldy5230411262 <rival.ganten99@gmail.com>
2024-12-26 10:22:09 +0700

    first commit

---
+++ app/app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from functools import wraps
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your-secret-key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
